b9ba9cd63c20792ebaa18ed7e8ffcdcbe6e24e0b
mrkbt <marek.bot@live.com>
2025-11-05 11:43:04 +0100

    refactoring into separate files

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def get_book_text(filepath):
+from stats import get_num_words
+
+
+def get_book_text(filepath: str):
     with open(filepath, "r") as file:
         file_contents = file.read()
         return file_contents
@@ -7,6 +10,7 @@ def get_book_text(filepath):
 def main():
     filepath = "./books/frankenstein.txt"
     print(get_book_text(filepath))
+    get_num_words(text=get_book_text(filepath=filepath))
 
 
 if __name__ == "__main__":
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text: str) -> None:
+    num_words = len(text.split())
+    print(f"Found {num_words} total words")
