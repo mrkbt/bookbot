cad4976030b90ff8fc1b738095a96fb093e80878
mrkbt <marek.bot@live.com>
2025-11-05 09:51:33 +0100

    modified main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, "r") as file:
+        file_contents = file.read()
+        return file_contents
+
+
+def main():
+    filepath = "./books/frankenstein.txt"
+    print(get_book_text(filepath))
+
+
+if __name__ == "__main__":
+    main()
