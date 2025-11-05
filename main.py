24fde518cd437734a35b788595c74c4ebe01d1e6
mrkbt <marek.bot@live.com>
2025-11-05 13:15:01 +0100

    finished project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words, get_num_characters, sort_num_characters
 
 
 def get_book_text(filepath: str):
@@ -8,9 +9,25 @@ def get_book_text(filepath: str):
 
 
 def main():
-    filepath = "./books/frankenstein.txt"
-    print(get_book_text(filepath))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     get_num_words(text=get_book_text(filepath=filepath))
+    print("--------- Character Count -------")
+
+    text = get_book_text(filepath)
+    num_characters = get_num_characters(text)
+    sorted_list = sort_num_characters(num_characters)
+    for item in sorted_list:
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
 def get_num_words(text: str) -> None:
     num_words = len(text.split())
     print(f"Found {num_words} total words")
+
+
+def get_num_characters(text: str) -> dict[str, int]:
+    results: dict[str, int] = {}
+    for character in text:
+        if not character.isalpha():
+            continue
+        character_lowercase = character.lower()
+        if character_lowercase in results.keys():
+            results[character.lower()] += 1
+        else:
+            results[character_lowercase] = 1
+
+    return results
+
+
+def sort_on(items: dict[str, int]) -> int:
+    return items["num"]
+
+
+def sort_num_characters(num_characters: dict[str, int]) -> list[dict[str, str | int]]:
+    result = [{"char": k, "num": v} for k, v in num_characters.items()]
+    result.sort(reverse=True, key=sort_on)
+    return result
